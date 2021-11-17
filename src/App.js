d1a9777b6a6d2d7d97c0ec94c9f8e383eff86db9
Thiru-cse <thiru.c.cse@gmail.com>
2021-11-17 17:32:28 -0500

    Updated🎉

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import PriceCard from './PriceCard';
+import './PriceCard.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <section class="pricing py-5">
+    <div class="container">
+    <div class="row">
+
+    <div class="col-lg-4">
+      <PriceCard title='Free' price='0' users='Single User' storage='5GB Storage'/>
+      </div>
+
+      <div class="col-lg-4">
+      <PriceCard title='Plus' price='9' users='5 User' storage='50GB Storage'/>
+      </div>
+
+      <div class="col-lg-4">
+      <PriceCard title='Pro' price='49' users='Unlimited Users' storage='150GB Storage' />
+      </div>
+      
     </div>
+    </div>
+    </section>
+    </>
   );
 }
 
---
+++ src/PriceCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './PriceCard.css';
+
+const PriceCard = (props) => {
+    return (
+        <>
+        
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">{props.title}</h5>
+            <h6 class="card-price text-center">${props.price}<span class="period">/month</span></h6>
+            <hr/>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>{props.users}</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>{props.storage}</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+              <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                Private Projects</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                Phone Support</li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+              </li>
+              <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                Reports</li>
+            </ul>
+            <div class="d-grid">
+              <a href="#" class="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+
+        </>
+    )
+}
+
+export default PriceCard
